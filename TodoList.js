1befe74b32c7ca86836fc26bc8701b1cf5c83736
leo424y <leo424y@gmail.com>
2016-08-10 18:47:21 +0800

    Level 8. 動態產生多個 React 元件

--- TodoList.js
+++ TodoList.js
@@ -2,31 +2,22 @@ const { TodoItem } = window.App;
 
 class TodoList extends React.Component {
   render() {
-    // 將 title 和 completed 依序傳遞給 TodoItem；
-    // 傳遞參數的方式，就如同定義 HTML 元素的屬性
-    return (
-      <ul>
-        <li>
-          <TodoItem
-            title="Item 1"
-            completed={true}
-          />
-        </li>
-        <li>
-          <TodoItem
-            title="Item 2"
-            completed={false}
-          />
-        </li>
-        <li>
-          <TodoItem
-            title="Item 3"
-            completed={false}
-          />
-        </li>
-      </ul>
-    );
+    const { todos } = this.props; // 1. 從 props 中，取得 todos (待辦清單) 陣列
+    // 2. 將每一筆項目轉成 li 元素，並塞入對應的待辦資料
+    //    PS. 務必給每筆 li 唯一 key（詳見[學習筆記 1]）
+    const todoElements = todos.map((todo) => (
+      <li key={todo.id}>
+        <TodoItem
+          title={todo.title}
+          completed={todo.completed}
+        />
+      </li>
+    ));
+    return <ul>{todoElements}</ul>;
   }
 }
+TodoList.propTypes = {
+  todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+};
 
 window.App.TodoList = TodoList;
