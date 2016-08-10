4e90806ced0f3bc6d78a57032090c97d55cd9898
leo424y <leo424y@gmail.com>
2016-08-10 20:58:41 +0800

    Revert "3.2 修改 TodoItem"
    This reverts commit 88dfbbce4f48eb05fda383a7fdeb96ce1cbc77bd.

--- TodoItem.js
+++ TodoItem.js
@@ -3,21 +3,15 @@ const { InputField } = window.App;
 class TodoItem extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    // 1. 使用 class constructor (類別建構子) 初始元件狀態
     this.state = { editable: false };
-
-    // 7. 在 ES6 component class 中，你必須手動綁定 this
     this.toggleEditMode = this.toggleEditMode.bind(this);
   }
 
   toggleEditMode() {
-    // 6. 更新元件狀態來切換模式
     this.setState({ editable: !this.state.editable });
   }
 
   renderViewMode() {
-    // 3. 將原本渲染「瀏覽模式」的程式，移至這裡
     const {
       title,
       completed,
@@ -35,15 +29,11 @@ class TodoItem extends React.Component {
   renderEditMode() {
     const { title } = this.props;
     return (
-      // 4. 「編輯模式」使用 InputField 元件
       <InputField
-        // 5. autoFocus 讓使用者切換到編輯模式後，可以立即編打
         autoFocus
         placeholder="編輯待辦事項"
         value={title}
-        onBlur={this.toggleEditMode} // 8. 當使用者點擊其他地方，則切換為「瀏覽模式」
-
-        // 9. 當使用者按下 ESC，則切換為「瀏覽模式」
+        onBlur={this.toggleEditMode}
         onKeyDown={(e) => {
           if (e.keyCode === 27) {
             e.preventDefault();
@@ -55,7 +45,6 @@ class TodoItem extends React.Component {
   }
 
   render() {
-    // 2. 判斷目前模式為何，渲染不同的畫面
     return this.state.editable ?
       this.renderEditMode() :
       this.renderViewMode();
